49926f7a2d23d93061f452ac5a42468af4256570
ARUN2704 <arunarun45674567@gmail.com>
2023-09-11 05:04:08 -0700

    addcss

---
+++ main.js
@@ -0,0 +1,37 @@
+$('.brand').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items:3
+        },
+        1000: {
+            items:6
+        }
+    }
+})
+
+
+$('.arun').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items:1
+        },
+        600: {
+            items:1
+        },
+        1000: {
+            items:1
+        }
+    }
+})
+
+
+
